67909af3cd6db11ecf3ab5599b9cf2f42b1658dc
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-10-13 13:32:44 -0500

    Hello World setup.

---
+++ ch2/hello.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My first PHP page</title>
+  </head>
+  <body>
+    <h1>Hello world!</h1>
+    <?php
+      #Simple hello world page
+      echo "Hello PHP!";
+    ?>
+    <ul>
+      <li><a href="/one-day-php/index.php">Course Index</a></li>
+    </ul>
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My first PHP page</title>
+  </head>
+  <body>
+    <h1>PHP in a Day Index</h1>
+    <?php
+      #Simple hello world page
+      echo "Hello World!";
+    ?>
+    <h3>Chapter 2</h3>
+    <ul>
+      <li><a href="/one-day-php/ch2/hello.php">hello.php</a></li>
+    </ul>
+  </body>
+</html>
