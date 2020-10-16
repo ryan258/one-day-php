d4b33d5cafd45a0359d2de1f9221ef8c3f1485f7
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-10-16 13:16:54 -0500

    Chapter 10: Object-Oriented Programming: Inheritance.

---
+++ ch10/chap10.php
@@ -0,0 +1,10 @@
+<?php
+	include 'AwardWinningMovie.php';
+	  $awm = new AwardWinningMovie('A12324', 'Max', 6.99, 'Best Picture');
+	
+	echo $awm->recommend('Japan');
+
+echo $awm->displayHeading('h1');
+
+$mv = new Movie('A3244', 'Golden Rose', 3.99);
+echo $mv->displayHeading('H1');
